b392aad50d82535e2475870092b1d88e65db82db
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-18 01:25:00 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
-def #display_rainbow(color)
+def display_rainbow(color)
   puts
   "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
