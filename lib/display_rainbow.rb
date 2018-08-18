1acc63759584d82e19a61c3e6497a2602e8991ca
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-18 01:35:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,6 @@
 #   puts
 #   "R: #{color[0]},O:#{color[1]},Y:#{color[2]},G:#{color[3]}, B:#{color[4]},I:#{color[5]},V:#{color[6]}"
 # end
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end 
