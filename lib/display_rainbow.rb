fc8cbb9c8090ffcf081e501554f574f90a33e587
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-18 01:30:52 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
   puts
-  "R: #{color[0]},#{color[1]},#{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]}"
+  "R: #{color[0]},#{color[1]},#{color[2]}, #{color[3]}, #{color[4]}, #{color[5]}, #{color[6]}\n"
 end
